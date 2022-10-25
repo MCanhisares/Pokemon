b9d9b0b556c0401d36513c159870f037445cde01
Marcel Canhisares <marcel.canhisares@gmail.com>
2022-10-25 20:02:16 -0300

    Initial commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Marcel Canhisares on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
